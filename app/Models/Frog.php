37b1f2c7b361afbc140302520abc35079d86ea2e
Victoria Herod <vicky_herod@live.com>
2021-01-11 18:07:14 +0000

    Duck model dummy data for view in mySQL, Frog model created

---
+++ app/Models/Frog.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Frog extends Model
+{
+    use HasFactory;
+}
--- database/migrations/2021_01_11_163921_create_ducks_table.php
+++ database/migrations/2021_01_11_163921_create_ducks_table.php
@@ -15,8 +15,8 @@ class CreateDucksTable extends Migration
     {
         Schema::create('ducks', function (Blueprint $table) {
             $table->id();
-            $table->string('regular-name', 100);
-            $table->string('latin-name', 100);
+            $table->string('regular_name', 100);
+            $table->string('latin_name', 100);
             $table->text('description');
             $table->enum('extroversion', [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
             $table->enum('colourfulness', [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
@@ -37,3 +37,24 @@ class CreateDucksTable extends Migration
         Schema::dropIfExists('ducks');
     }
 }
+
+
+// $table->string('regular-name', 100);
+// $table->string('latin-name', 100);
+// $table->text('description');
+// $table->enum('extroversion', [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+// $table->enum('colourfulness', [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+// $table->enum('kindness', [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+// $table->enum('activeness', [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+// $table->enum('happiness', [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+
+// $duck = new Duck();
+// $duck->regular_name = 'Ducko';
+// $duck->latin_name = 'Duckidus Donaldson';
+// $duck->description = 'What a quacking loving hoonan!';
+// $duck->extroversion = 7;
+// $duck->colourfulness = 4;
+// $duck->kindness = 9;
+// $duck->activeness = 6;
+// $duck->happiness = 10;
+// $duck->save();
