c8128826aa2d9224798754391077caa0f52759c9
Victoria Herod <vicky_herod@live.com>
2021-01-11 17:45:55 +0000

    Extra fields added to duck table

--- database/migrations/2021_01_11_163921_create_ducks_table.php
+++ database/migrations/2021_01_11_163921_create_ducks_table.php
@@ -15,6 +15,14 @@ class CreateDucksTable extends Migration
     {
         Schema::create('ducks', function (Blueprint $table) {
             $table->id();
+            $table->string('regular-name', 100);
+            $table->string('latin-name', 100);
+            $table->text('description');
+            $table->enum('extroversion', [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+            $table->enum('colourfulness', [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+            $table->enum('kindness', [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+            $table->enum('activeness', [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+            $table->enum('happiness', [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
             $table->timestamps();
         });
     }
