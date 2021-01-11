b5d17cbdece27abcfa17582a3e10219fc7e3191b
Victoria Herod <vicky_herod@live.com>
2021-01-11 18:56:34 +0000

    Test data in both duck and frog tables, empty get request created

--- database/migrations/2021_01_11_180638_create_frogs_table.php
+++ database/migrations/2021_01_11_180638_create_frogs_table.php
@@ -15,6 +15,14 @@ class CreateFrogsTable extends Migration
     {
         Schema::create('frogs', function (Blueprint $table) {
             $table->id();
+            $table->string('regular_name', 100);
+            $table->string('latin_name', 100);
+            $table->text('description');
+            $table->enum('extroversion', [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+            $table->enum('colourfulness', [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+            $table->enum('kindness', [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+            $table->enum('activeness', [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+            $table->enum('happiness', [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
             $table->timestamps();
         });
     }
@@ -29,3 +37,14 @@ class CreateFrogsTable extends Migration
         Schema::dropIfExists('frogs');
     }
 }
+
+// $frog = new Frog();
+// $frog->regular_name = 'Chief Ribbet';
+// $frog->latin_name = 'Ribbet o Ribbins';
+// $frog->description = 'A subtle but spiritually vibrant froggo';
+// $frog->extroversion = 5;
+// $frog->colourfulness = 9;
+// $frog->kindness = 10;
+// $frog->activeness = 3;
+// $frog->happiness = 8;
+// $frog->save();
